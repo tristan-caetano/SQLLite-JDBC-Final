0d0822764394a8128475504bfd424a160884b480
Tristan Caetano <tcaetano@localhost.localdomain>
2021-12-04 19:28:26 -0500

    Added accident search

--- src/net/codejava/JavaSQL.java
+++ src/net/codejava/JavaSQL.java
@@ -5,25 +5,44 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.ResultSet;
+import java.util.Scanner;
 
 public class JavaSQL {
 
     public static void main(String[] args){
-        String jdbcURL = "jdbc:sqlite:/home/tcaetano/Projects/CIS454Final/autosDB.sqlite";
-        try{
-            Connection connection = DriverManager.getConnection(jdbcURL);
-            //String sql = "SELECT * FROM people";
-            String sql = "SELECT * FROM people";
+        //String jdbcURL = "jdbc:sqlite:/home/tcaetano/Projects/CIS454Final/autosDB.sqlite";
+        accidents("jdbc:sqlite:/home/tcaetano/Projects/CIS454Final/autosDB.sqlite");
+    }
 
+    public static void accidents(String url){
+        System.out.println("Please input an accident ID");
+        Scanner in = new Scanner(System.in);
+        int id = in.nextInt();
+
+        try{
+            Connection connection = DriverManager.getConnection(url);
+            String sql = "SELECT accidents.aid, accident_date, city, state, involvements.vin, damages, involvements.driver_ssn " +
+                         "FROM accidents " +
+                         "JOIN involvements ON accidents.aid = involvements.aid " +
+                         "WHERE accidents.aid = " + id;
 
             Statement statement = connection.createStatement();
             ResultSet result = statement.executeQuery(sql);
 
             while(result.next()){
-                String fname  = result.getString("fname");
-                String lname  = result.getString("lname");
+                String date  = result.getString("accident_date");
+                String city  = result.getString("city");
+                String state  = result.getString("state");
+                String vin  = result.getString("vin");
+                String damages  = result.getString("damages");
+                String ssn  = result.getString("driver_ssn");
 
-                System.out.println(fname + "|" + lname);
+                System.out.println("Date: " + date +
+                                   "\nLocation: " + city + ", " + state +
+                                   "\nVin #: " + vin +
+                                   "\nDamages: $" + damages +
+                                   "\nDriver SSN: " + ssn);
+                break;
             }
 
         }catch(SQLException e){
